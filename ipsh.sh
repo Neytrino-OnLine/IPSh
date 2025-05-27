f6bc29c50b4627c856b85fe703b57c6f5fc0ad83
Neytrino-OnLine <33822181+Neytrino-OnLine@users.noreply.github.com>
2025-05-27 07:54:59 +0300

    Update ipsh.sh
    - Адаптация к изменениям на сайте IPSpeed...

--- ipsh.sh
+++ ipsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="beta 1"
+VERSION="beta 1.1"
 PROFILE_PATH="/opt/etc/ipsh/ipsh.conf"
 TABLE_FILE='/tmp/ipspeed.tbl'
 LIST_FILE='/tmp/ipspeed.lst'
@@ -222,7 +222,8 @@ function newList
 	fi
 	local TEST=`elinks -source https://ipspeed.info/freevpn_sstp.php | grep -c "vpn"`
 	if [ "$TEST" -gt "0" ];then
-		local TABLE=`elinks -source https://ipspeed.info/freevpn_sstp.php?language=en | awk '{gsub(/<\/div>/,"\t")}1' | awk '{gsub(/ ms/," ms\n")}1' | sed  's/<[^>]*>//g' | awk '{gsub(/^\t\t\t/,"@")}1' | awk '{gsub(/^\t\t/,"@")}1'  | awk '{gsub(/^LOCATION\tHOSTNAME\tUPTIME\tPING\t\t\t/,"")}1' | grep "^@." | grep -v "\"" | awk '{gsub(/^@/,"")}1' | grep -v "Russian Federation\|Ukraine" | grep " ms" | awk '{gsub(/ ms/,"")}1'  | awk '{gsub(/^\t/,"")}1'`
+		local TABLE=`elinks -source https://ipspeed.info/freevpn_sstp.php?language=en | awk '{gsub(/<\/p>/,"\t"); gsub(/ ms/," ms\n")}1' | tr '. ' '@'  | sed  's/<[^>]*>//g'  | grep "@@" | grep -v "^@@\|^Username" | awk -F"@@" '{print $2}' | tr '@' '.'`
+		#elinks -source https://ipspeed.info/freevpn_sstp.php?language=en | awk '{gsub(/<\/div>/,"\t")}1' | awk '{gsub(/ ms/," ms\n")}1' | sed  's/<[^>]*>//g' | awk '{gsub(/^\t\t\t/,"@")}1' | awk '{gsub(/^\t\t/,"@")}1'  | awk '{gsub(/^LOCATION\tHOSTNAME\tUPTIME\tPING\t\t\t/,"")}1' | grep "^@." | grep -v "\"" | awk '{gsub(/^@/,"")}1' | grep -v "Russian Federation\|Ukraine" | grep " ms" | awk '{gsub(/ ms/,"")}1'  | awk '{gsub(/^\t/,"")}1'
 		local TEST=`echo "$TABLE" | grep -c $`
 		if [ "$TEST" -gt "0" ];then
 			echo "$TABLE" > $TABLE_FILE
